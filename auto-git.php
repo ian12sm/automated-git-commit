60d0087529e0946d8bf585aae477fed41559c4fb
iannoyes <iannoyesdev@gmail.com>
2016-01-11 17:33:25 -0600

    iterate through dirs and check for changes

--- auto-git.php
+++ auto-git.php
@@ -1,17 +1,68 @@
 <?php
-
+use GitWrapper\GitWrapper;
 	/* This program uses the the git wrapper found at https://github.com/cpliakas/git-wrapper */
 
 	//get dependencies
-	require __DIR__ . '/vendor/autoload.php';
+	require_once __DIR__ . '/vendor/autoload.php';
 
 	//include our config file
 	include 'config.php';
 
+	//use our gitwrapper
+	
+
 	//get directories
 	$dirs = glob(APPS, GLOB_ONLYDIR);
 
-	print_r($dirs);
+	//loop through the dirs
+	function iterate_dirs($dirs){
+
+		if ($dirs) {
+			
+			foreach ($dirs as $dir) {
+
+				$wrapper = new GitWrapper();
+
+				//find .git file in dir
+				if (file_exists($dir .'/public/.git/')) {
+					$dir .= '/public';
+				} elseif (file_exists($dir . '/.git')) {
+					//do nothing
+				} else {
+					echo($dir . 'does not contain a git repository');
+
+					//php returns fatal errors so skip this if no repo
+					continue;
+				}
+
+/*				$wrapper->setEnvVar('HOME', '/path/to/a/private/writable/dir');*/
+				
+					$copy = $wrapper->workingCopy($dir);
+
+					if ($copy->hasChanges()) {
+						echo $dir . " Has Ch-Ch-Ch-Ch-Ch-Changes!";
+						echo "\n";
+					} else {
+						echo $dir . " Does not have changes!";
+						echo "\n";
+					}
+
+				$status = $wrapper->git('--git-dir=' . $dir .'/.git --work-tree=' . $dir . ' status');
+
+			}
+
+		} else{
+			//die if apps directory is not propery configured.
+			die("APPS is not properly configued.");
+		}
+
+	}
+
+	iterate_dirs($dirs);
+
+	function git_status(){
+		//check dir for get status and shit
+	}
 
 
 ?>
