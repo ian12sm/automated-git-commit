91377a32b96e800d2a51462c224d6b03f90f59f6
Ian Noyes <ian@12southmusic.com>
2016-02-27 19:42:54 -0600

    logging and email

--- auto-git.php
+++ auto-git.php
@@ -6,9 +6,7 @@
 
 	//use libraries
 	use GitWrapper\GitWrapper;
-	use GitWrapper\Event\GitLoggerListener;
-	use Monolog\Logger;
-	use Monolog\Handler\StreamHandler;
+	use Mailgun\Mailgun;
 
 	//include our config file
 	include 'config.php';
@@ -22,10 +20,13 @@
 	//loop through the dirs
 	function iterate_dirs($dirs){
 
+		//create status
+		$status = '';
+
 		if ($dirs) {
 			
-			foreach ($dirs as $dir) {
-
+			foreach ($dirs as $dir) 
+{
 				$wrapper = new GitWrapper();
 
 				//find .git file in dir
@@ -34,43 +35,95 @@
 				} elseif (file_exists($dir . '/.git')) {
 					//do nothing
 				} else {
-					echo($dir . 'does not contain a git repository');
+					echo($dir . ' does not contain a git repository');
 
 					//php returns fatal errors so skip this if no repo
 					continue;
 				}
-
-/*				$wrapper->setEnvVar('HOME', '/path/to/a/private/writable/dir');*/
 				
+					$git_time = date('[m:d:y](G:i:s)');
+					//start the time
+					$status .= $git_time;
+
 					$copy = $wrapper->workingCopy($dir);
 
 					if ($copy->hasChanges()) {
-					
-						$status = $wrapper->git('--git-dir=' . $dir .'/.git --work-tree=' . $dir . ' status');
-					
-						print_r($status);
+
+						$status .= "\n";
+
+						$status .= "\n";
+
+						$status .="Changes in " . $dir;
+
+						$status .= "\n";
+
+						$status .= git_status($wrapper, $dir);
+
+						git_commit($wrapper, $dir, $git_time);
+
 
 					} else {
-						echo $dir . " Does not have changes!";
-						echo "\n";
+						$status .=  $dir . " Does not have changes!";
 					}
 
+					$status .= "\n";
+
+					$status .= "\n";
+
+					
 				//$status = $wrapper->git('--git-dir=' . $dir .'/.git --work-tree=' . $dir . ' status');
 
 			}
+
+
+			
+			send_mail_log($status);
 
 		} else{
 			//die if apps directory is not propery configured.
 			die("APPS is not properly configued.");
 		}
 
+
 	}
 
-	iterate_dirs($dirs);
-
-	function git_status(){
+	function git_commit($wrapper, $dir, $git_time){
 		//check dir for get status and shit
+		
+			$wrapper->git('--git-dir=' . $dir .'/.git --work-tree=' . $dir . ' add --all' );
+		
+			$wrapper->git('--git-dir=' . $dir .'/.git --work-tree=' . $dir . ' commit -m "auto commit at ' . $git_time . '"' );
+
 	}
 
+	function git_status($wrapper, $dir){
 
+		$status = $wrapper->git('--git-dir=' . $dir .'/.git --work-tree=' . $dir . ' status');
+	
+		return $status;
+
+	}
+
+	//sent an email
+
+	function send_mail_log($email_content){	
+
+		# First, instantiate the SDK with your API credentials and define your domain. 
+		$mg = new Mailgun("key-15i5a455-ed224utmcx6k5ck84zkx6w7");
+		$domain = "12southmusic.com";
+
+		$subject = SUBJECT . date('[m:d:y](G:i:s)');
+
+		# Now, compose and send your message.
+		$mg->sendMessage($domain, array('from'    => EMAIL_FROM, 
+		                                'to'      => EMAIL_TO_1, 
+		                                'subject' => $subject, 
+		                                'text'    => $email_content));
+
+    }
+
+
+
+     //fire everything
+     iterate_dirs($dirs);
 ?>
---
+++ config-example.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This is an example configuration file
+ * Copy this file to 'config.php' and change the globals to match your server environment
+*/ 
+
+//use the absolute path to the directories you want to check
+define('APPS', '/home/ian/apps/*/public');
+
+
+
+
+
+
+?>
